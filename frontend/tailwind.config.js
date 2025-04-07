649ff380e7347e99cffdf5e64cdbfd8c1b5d332c
k4west <kwestmin@gmail.com>
2025-04-08 01:40:23 +0900

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}', // 사용되는 파일 경로에 맞게 수정
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
